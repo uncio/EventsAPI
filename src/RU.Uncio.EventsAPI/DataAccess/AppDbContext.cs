c7f6fa6b73a112c50b1dba5990ff0833f2df32b3
Katerina <unciocat@hotmail.com>
2026-05-21 09:51:17 +0300

    add db context

---
+++ src/RU.Uncio.EventsAPI/DataAccess/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RU.Uncio.EventsAPI.Models;
+
+namespace RU.Uncio.EventsAPI.DataAccess
+{
+    /// <summary>
+    /// DataBase context
+    /// </summary>
+    public class AppDbContext: DbContext
+    {
+        private readonly ILogger<AppDbContext> logger;
+
+        /// <summary>
+        /// Events table
+        /// </summary>
+        public DbSet<Event> Events => Set<Event>();
+        /// <summary>
+        /// Bookings table
+        /// </summary>
+        public DbSet<Booking> Bookings => Set<Booking>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="opts"></param>
+        /// <param name="log"></param>
+        public AppDbContext(DbContextOptions<AppDbContext> opts, ILogger<AppDbContext> log)
+            :base(opts)
+        {
+            logger = log;
+        }
+
+        /// <summary>
+        /// On model creating setup
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
--- src/RU.Uncio.EventsAPI/Models/Event.cs
+++ src/RU.Uncio.EventsAPI/Models/Event.cs
@@ -37,7 +37,7 @@
         /// <summary>
         /// Bookings for the event
         /// </summary>
-        public List<Guid> Bookings { get; set; }
+        public List<Guid> Bookings { get; set; } = new();
 
         /// <summary>
         /// For deserialzer
